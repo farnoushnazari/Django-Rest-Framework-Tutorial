25f5a4f22394b296defb55c6852f4d526dd803e0
farnoushnazari <farnoushnazari98@gmail.com>
2025-07-25 12:39:01 +0330

    DRF CreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductDetailAPIView
+from .views import ProductDetailAPIView, ProductCreateAPIView
 
 urlpatterns = [
+    path('', ProductCreateAPIView.as_view()),
     path('<int:pk>/', ProductDetailAPIView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,16 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perfom_create(self, serializer):
+        title = serializer.validate_data.get('title')
+        content = serializer.validate_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
